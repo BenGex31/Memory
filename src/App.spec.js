609b7e2ef66b933a869f5a04cf33ba402d683347
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-11-27 16:00:20 +0100

    tests written with Chai/ Enzyme / Jest / Sinon

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,8 +1,19 @@
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
 
-it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
+import App from './App'
+import GuessCount from './GuessCount'
+
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />)
+
+        expect(wrapper).to.contain(<GuessCount guesses={0}/>)
+    })
+    it('has 36 cards', () => {
+        const wrapper = shallow(<App />)
+
+        expect(wrapper.find('Card')).to.have.length(36)
+    })
 })
